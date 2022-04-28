abde93f44ecb41de2149e7beacfd2966d89b6010
sammy-jay <sammyjayden27@gmail.com>
2022-04-28 05:11:11 +0100

    response problem fixed

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,14 +2,14 @@ import * as api from "../api";
 
 export const signIn = (credentials) => async (dispatch) => {
   try {
-    const { response } = await api.signIn(credentials);
+    const { data } = await api.signIn(credentials);
     localStorage.setItem(
       "access_token",
-      JSON.stringify(response.data.access_token)
+      JSON.stringify(data.data.access_token)
     );
-    localStorage.setItem("user", JSON.stringify(response.data.user));
-    dispatch({ type: "AUTH", payload: response.data });
-    if (response.status === "true") return true;
+    localStorage.setItem("user", JSON.stringify(data.data.user));
+    dispatch({ type: "AUTH", payload: data.data });
+    if (data.status === "true") return true;
   } catch (error) {
     console.log(error);
   }
@@ -17,24 +17,24 @@ export const signIn = (credentials) => async (dispatch) => {
 
 export const signUp = (credentials) => async (dispatch) => {
   try {
-    const { response } = await api.signUp(credentials);
+    const { data } = await api.signUp(credentials);
     console.log("Step 1");
     localStorage.setItem(
       "access_token",
-      JSON.stringify(response.data.access_token)
+      JSON.stringify(data.data.access_token)
     );
     console.log("Step 2");
     const newUser = {
-      fullname: response.data.user.fullname,
-      email: response.data.user.email,
-      username: response.data.user.username,
+      fullname: data.data.user.fullname,
+      email: data.data.user.email,
+      username: data.data.user.username,
     };
     console.log("Step 3");
     localStorage.setItem("user", JSON.stringify(newUser));
     console.log("Step 4");
-    dispatch({ type: "AUTH", payload: response.data.user });
+    dispatch({ type: "AUTH", payload: data.data.user });
     console.log("Step 5");
-    if (response.status === "true") return true;
+    if (data.status === "true") return true;
     console.log("Step 6");
   } catch (error) {
     console.log(error);
