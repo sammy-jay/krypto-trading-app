867a2d8d7f1aff1643b5b889372d81c141a7d71b
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-05-02 13:24:42 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -7,9 +7,7 @@ const middleware = [thunk]
 const store = createStore(
     reducer,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && 
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
     )
 );
 
